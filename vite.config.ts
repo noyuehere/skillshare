7fe411df3dcf413bcc48f899c2c65c5b83282347
月 <urnotdeva@gmail.com>
2026-06-07 16:26:14 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+
+    base: "/noyuehere/",
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
